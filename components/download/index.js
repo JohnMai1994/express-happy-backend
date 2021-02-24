4fb38ee9a8025a8a82972616b07849eea86780d4
John Mai <44597440+JohnMai1994@users.noreply.github.com>
2021-02-23 20:53:32 -0500

    下载方法二

--- components/download/index.js
+++ components/download/index.js
@@ -45,7 +45,20 @@ downloadRouter.route("/method1")
         res.send("download Delete");
     });
 
+// 方法二: 使用fs方法，读取文件，然后通过stream下载
+downloadRouter.route("/method2")
+    .get((req, res) => {
+        let fileName = req.param("name")
+        let file = `${__dirname}/assert/${fileName}`
+        console.log("下载文件地址: " + file)
+        let mimeType = mime.getType(file);
+        console.log(mimeType)
 
+        res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
+        res.setHeader('Content-type', mimeType);
+        let fileReadStream = fs.createReadStream(file);
+        fileReadStream.pipe(res)
+    })
 
 
 
